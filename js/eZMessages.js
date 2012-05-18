77d8c70d36a94da03b3001bbbcba3d9bdfae18e6
Konrad Dzwinel <kdzwinel@gmail.com>
2012-05-18 13:43:11 +0200

    Default settings moved to storeInit. eZMessages.js doesn't need to load settings from background page, it has access to extensions localStorage.

--- js/eZMessages.js
+++ js/eZMessages.js
@@ -1,36 +1,32 @@
 var previousMessagesList;
 var messagesList;
-var settings;
+
+console.log(settings.toObject());
 
 function init() {
 	console.log('eZMessages init');
-	
-	//reload settings
-	chrome.extension.sendRequest({command: 'getSettings'}, function(theSettings) {
-		settings = new Store("settings", theSettings);
 		
-		//ask content script for data
-		chrome.extension.sendRequest({tabId: chrome.devtools.inspectedWindow.tabId, command: "getMessages"}, function(messages) {
-			console.log('eZMessages getMessages');
-			
-			if( messagesList ) {
-				previousMessagesList = messagesList;
-			}
-			
-			messagesList = new MessagesList();
-			messagesList.setMessages(messages);
-			
-			//show difference between old message log and a new one
-			if( previousMessagesList ) {
-				messagesList.highlightNewMessages( previousMessagesList );
-			}
-			
-			//display data
-			$('#debug_toolbar').html(messagesList.render());
-			
-			//highlight new messages (that are not hidden by filtering)
-			$('#debug_toolbar .debug_messages li.is_new:visible').effect('highlight', {}, 2500);
-		});
+	//ask content script for data
+	chrome.extension.sendRequest({tabId: chrome.devtools.inspectedWindow.tabId, command: "getMessages"}, function(messages) {
+		console.log('eZMessages getMessages');
+		
+		if( messagesList ) {
+			previousMessagesList = messagesList;
+		}
+		
+		messagesList = new MessagesList();
+		messagesList.setMessages(messages);
+		
+		//show difference between old message log and a new one
+		if( previousMessagesList ) {
+			messagesList.highlightNewMessages( previousMessagesList );
+		}
+		
+		//display data
+		$('#debug_toolbar').html(messagesList.render());
+		
+		//highlight new messages (that are not hidden by filtering)
+		$('#debug_toolbar .debug_messages li.is_new:visible').effect('highlight', {}, 2500);
 	});
 }
 
---
+++ js/storeInit.js
@@ -0,0 +1,19 @@
+//create a storage object and load default settings
+var settings = new Store("settings", {
+	'hideeZDebug': 'hide_used',
+	'labelColorError': '#E2725B',
+	'labelColorWarning': '#E3A857',
+	'labelColorNotice': '#FADA5E',
+	'labelColorDebug': '#99BADD',
+	'labelColorTiming': '#00A86B',
+	'labelColorTemplateHead': '#EDC951',
+	'labelColorTemplateBody': '#EB6841',
+	'labelColorTemplateDefault': '#00A0B0',
+	'showTemplatePath': 'requested',
+	
+	'errorMessageVisible': true,
+	'warningMessageVisible': true,
+	'noticeMessageVisible': true,
+	'debugMessageVisible': true,
+	'timingMessageVisible': false
+});
